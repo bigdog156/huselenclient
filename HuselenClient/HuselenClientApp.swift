1c35566a878e40f54aa3702a1757b972a2cdf061
lamlt <lam.le@finos.asia>
2025-12-17 11:05:49 +0700

    Initial Commit

---
+++ HuselenClient/HuselenClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuselenClientApp.swift
+//  HuselenClient
+//
+//  Created by Le Thach lam on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HuselenClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
